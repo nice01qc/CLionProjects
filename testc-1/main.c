0f206a3c21a0ee9d50240afe9a97244c497b4418
ljh <734522310@qq.com>
2018-01-24 14:10:02 +0800

    update

--- testc-1/main.c
+++ testc-1/main.c
@@ -1,18 +1,21 @@
-#include "one/first.h"
 #include <stdio.h>
-#include <direct.h>
-void main2(char *);
-int main(void) {
+#include "one/first.h"
 
+struct student{
+    int age;
+    char name[30];
+};
+typedef struct{
+    int age;
+} Stu;
+int main(void) {
 
 //    main1("../testtxt");
 
-    main2("../one/main2txt");
-    char buffer[1024];
-    _getcwd(buffer,1024);
-    printf("%s",buffer);
+//    main2("../one/main2txt");
+
+    allfault();
 
 
     return 0;
 }
-
